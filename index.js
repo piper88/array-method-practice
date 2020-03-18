e3fa56be09ebdf39d6ac33837138fc548f6fd3ba
Sarah DeBey <sarah.debey@gmail.com>
2020-03-18 11:56:46 -0700

    adds shift and unshift methods

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const easy = require('./lib/easy.js')
 
 let array1 = [1,2,3,4,5];
 
-// console.log('are you there God, it\'s me Margaret');
+console.log('are you there God, it\'s me Margaret');
 
 console.log(easy.arrayPush(array1, 2));
 
-//because arrayPush modifies original array, so have to reassign to original
+// because arrayPush modifies original array, so have to reassign to original
 array1 = [1,2,3,4,5];
 
 console.log(`after second push ${easy.arrayPush(array1, 2)}`);
@@ -16,3 +16,11 @@ console.log(`after second push ${easy.arrayPush(array1, 2)}`);
 array1 = [1,2,3,4,5];
 
 console.log(`after pop ${easy.arrayPop(array1)}`);
+
+array1 = [1,2,3,4,5];
+
+console.log(`after shift ${easy.arrayShift(array1)}`);
+
+array1 = [1,2,3,4,5];
+
+console.log(`after unshift ${easy.arrayUnshift(array1, 0)}`);
